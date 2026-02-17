8da757f1e97504b850613cac1fe2f5970d391010
Douglas Rintoul <dougirintoul@gmail.com>
2026-02-17 16:48:41 +1100

    feat: Implement duplicate add checks and the removal functionality in addressBookSlice

--- src/core/reducers/addressBookSlice.ts
+++ src/core/reducers/addressBookSlice.ts
@@ -18,11 +18,12 @@ export const addressBookSlice = createSlice({
   initialState,
   reducers: {
     addAddress: (state, action: PayloadAction<Address>) => {
-      /** TODO: Prevent duplicate addresses */
-      state.addresses.push(action.payload);
+      if (!state.addresses.some(x => x.id === action.payload.id)) {
+        state.addresses.push(action.payload);
+      }
     },
     removeAddress: (state, action: PayloadAction<string>) => {
-      /** TODO: Write a state update which removes an address from the addresses array. */
+      state.addresses = state.addresses.filter(x => x.id !== action.payload);
     },
     updateAddresses: (state, action: PayloadAction<Address[]>) => {
       state.addresses = action.payload;
